4880db9bfef96f4c2b6012a8c0f616d77cd24f70
Cap_Honza <capjan.work@gmail.com>
2026-06-25 16:15:15 +0200

    Instalačka Reactu s Vitem

---
+++ React_sandbox/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Tady začíná moje čistá aplikace!</h1>
+      <p>Ten obří kód je pryč.</p>
+    </div>
+  )
+}
+
+export default App
